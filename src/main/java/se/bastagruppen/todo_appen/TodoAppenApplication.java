8b2aa6b4b459fc91c5e561e38ee0efe675dd9e1a
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2026-01-12 09:59:51 +0100

    Initial commit

---
+++ src/main/java/se/bastagruppen/todo_appen/TodoAppenApplication.java
@@ -0,0 +1,13 @@
+package se.bastagruppen.todo_appen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/bastagruppen/todo_appen/TodoAppenApplicationTests.java
@@ -0,0 +1,13 @@
+package se.bastagruppen.todo_appen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
