59807e52e78afdc3f519288fc23f46c4245588ab
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2026-01-15 13:34:18 +0100

    + created ToDoList class + added Tag class

--- src/main/java/se/bastagruppen/todo_appen/model/ToDoList.java
+++ src/main/java/se/bastagruppen/todo_appen/model/ToDoList.java
@@ -1,12 +1,24 @@
 package se.bastagruppen.todo_appen.model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Entity
+@Table(name = "todo_lists")
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class ToDoList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String name;
+
+    @OneToMany
+    @JsonManagedReference
+    private List <Tag> tags;
 }
