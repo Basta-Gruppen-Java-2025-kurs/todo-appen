3ba97af73d8afc621f47d8677743f09692bb25b0
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2026-01-15 10:54:27 +0100

    + created ToDoList class

---
+++ src/main/java/se/bastagruppen/todo_appen/model/ToDoList.java
@@ -0,0 +1,12 @@
+package se.bastagruppen.todo_appen.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ToDoList {
+
+}
