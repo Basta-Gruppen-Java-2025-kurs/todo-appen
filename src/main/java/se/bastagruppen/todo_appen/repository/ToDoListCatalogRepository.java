c8cc9612db156967cdf412bbc419449dc8eb1b04
unknown <sara.kluge@hotmail.com>
2026-01-15 10:27:03 +0100

    Add entity ToDoListCatalog and ToDoListCatalogRepository

---
+++ src/main/java/se/bastagruppen/todo_appen/repository/ToDoListCatalogRepository.java
@@ -0,0 +1,13 @@
+package se.bastagruppen.todo_appen.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.bastagruppen.todo_appen.entity.ToDoListCatalog;
+
+import java.util.List;
+
+public interface ToDoListCatalogRepository extends JpaRepository<ToDoListCatalog, Long> {
+
+    boolean existsByUserIdAndName(Long userId, String name);
+
+    List<ToDoListCatalog> findAllByUserId(Long userId);
+}
