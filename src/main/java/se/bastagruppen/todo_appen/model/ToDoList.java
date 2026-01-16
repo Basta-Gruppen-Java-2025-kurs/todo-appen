d5229692da59f2a14ca954c43a53dc29214ada1e
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2026-01-16 15:08:30 +0100

    Feature/todolist model class (#41)
    * add ToDoListEntity
    
    * add temporary User entity
    
    * + ToDoList and Tag entities
    
    * + ToDoList and Tag entities with User and ToDoListCatalog references
    
    * * ToDoList id int -> Long
    
    ---------
    
    Co-authored-by: unknown <sara.kluge@hotmail.com>

--- src/main/java/se/bastagruppen/todo_appen/model/ToDoList.java
+++ src/main/java/se/bastagruppen/todo_appen/model/ToDoList.java
@@ -14,7 +14,7 @@ import java.util.Set;
 public class ToDoList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String name;
 
