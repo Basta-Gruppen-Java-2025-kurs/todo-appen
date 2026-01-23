f9c8693bee971a2ea88267dfb2693cf15d265af1
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2026-01-23 12:57:34 +0100

    + Setting up controller test for ToDoList

---
+++ src/test/java/se/bastagruppen/todo_appen/controller/ToDoListControllerTest.java
@@ -0,0 +1,35 @@
+package se.bastagruppen.todo_appen.controller;
+
+import org.mapstruct.factory.Mappers;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import se.bastagruppen.todo_appen.mapper.ToDoListMapper;
+import se.bastagruppen.todo_appen.repository.ToDoListRepository;
+import se.bastagruppen.todo_appen.service.ToDoListService;
+import tools.jackson.databind.ObjectMapper;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class ToDoListControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ToDoListRepository repository;
+    @MockitoBean
+    private ToDoListService service;
+
+    @Spy
+    ToDoListMapper mapper = Mappers.getMapper(ToDoListMapper.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+}
