407a84a7799f0ba632899ec2260797cad82b88ae
Lenanor <nordstromlena@hotmail.com>
2026-01-29 13:19:56 +0100

    add custom principal, remove hardcoded user from entry controller

--- src/main/java/se/bastagruppen/todo_appen/controller/ToDoListEntryController.java
+++ src/main/java/se/bastagruppen/todo_appen/controller/ToDoListEntryController.java
@@ -2,11 +2,12 @@ package se.bastagruppen.todo_appen.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import se.bastagruppen.todo_appen.dto.ToDoListEntryRequestDto;
 import se.bastagruppen.todo_appen.dto.ToDoListEntryResponseDto;
+import se.bastagruppen.todo_appen.security.CustomPrincipal;
 import se.bastagruppen.todo_appen.service.ToDoListEntryService;
 
 import java.util.List;
@@ -18,16 +19,21 @@ public class ToDoListEntryController {
     private final ToDoListEntryService service;
 
     @PostMapping("/lists/{listId}/entries")
-    public ResponseEntity<ToDoListEntryResponseDto> createToDoListEntry(@PathVariable Long listId, @Valid @RequestBody ToDoListEntryRequestDto dto) {
-        // TODO: Change hard coded user id to logged in user
-        return ResponseEntity.ok(service.createToDoListEntry(listId, dto, 1L));
+    public ResponseEntity<ToDoListEntryResponseDto> createToDoListEntry(
+            @PathVariable Long listId,
+            @Valid @RequestBody ToDoListEntryRequestDto dto,
+            @AuthenticationPrincipal CustomPrincipal user) {
+        System.out.println(user.getUserId());
+        return ResponseEntity.ok(service.createToDoListEntry(listId, dto, user.getUserId()));
     }
 
     // GET all entries including subentries for a list
     @GetMapping("/lists/{listId}/entries")
-    public ResponseEntity<List<ToDoListEntryResponseDto>> getAllEntriesOfAList(@PathVariable Long listId) {
-        // TODO: Change hard coded user id to logged in user
-        return ResponseEntity.ok(service.getAllEntriesOfAList(listId, 1L));
+    public ResponseEntity<List<ToDoListEntryResponseDto>> getAllEntriesOfAList(
+            @PathVariable Long listId,
+            @AuthenticationPrincipal CustomPrincipal user) {
+        System.out.println(user.getUserId());
+        return ResponseEntity.ok(service.getAllEntriesOfAList(listId, user.getUserId()));
     }
 
     // GET /entries/{entryId}
@@ -36,9 +42,8 @@ public class ToDoListEntryController {
 
     // DELETE entry
     @DeleteMapping("/entries/{entryId}")
-    public ResponseEntity<Void> deleteEntryById(@PathVariable Long entryId) {
-        // TODO: Change hard coded user id to logged in user
-        service.deleteEntryById(entryId, 1L);
+    public ResponseEntity<Void> deleteEntryById(@PathVariable Long entryId, @AuthenticationPrincipal CustomPrincipal user) {
+        service.deleteEntryById(entryId, user.getUserId());
 
        return ResponseEntity.noContent().build();
     }
